d6a6b0e73a21213a1aa2a04c82f991691f078e7e
aneilshah <aneil.shah@esg-usa.com>
2019-07-15 08:53:16 -0400

    object model refined

--- app/controllers/loop.js
+++ app/controllers/loop.js
@@ -4,8 +4,8 @@ import {Obj} from '../obj';
 
 export default Controller.extend({
   count:0,
-  loopPerSec:50,
-  loopMS: 20,
+  framePerSec:50,
+  frameMS: 20,
   isRunning: false,
   objInit: false,
   left:0,
@@ -17,43 +17,46 @@ export default Controller.extend({
   obj3: new Obj,
 
   actions:{
-    start() {
-      this.set('isRunning',true);
-      if (!this.get('objInit'))
-      {
-        $('#graphContainer').append('<div' +
-          ' id="cm" style="left: 800px; top: 350px;' +
-          ' width: 10px; height: 10px;' +
-          ' border-radius: 2px; ' +
-          ' background-color: darkblue;' +
-          ' position: absolute;">' +
-          '</div>');
-        this.set('objInit',true);
-      }
+    reset(){
+      this.set('isRunning',false);
+      let fps=this.get('framePerSec');
       let obj1 = this.get('obj1');
       obj1.setID('one');
       obj1.setPos(1200,300);
-      obj1.setVel(0.0,0.5);
+      obj1.setVel(0.0,10);
       obj1.setColor('rgba(200, 41, 199, 0.5)');
       obj1.setMass(1);
       obj1.bind();
+      obj1.move(fps);
 
       let obj2 = this.get('obj2');
       obj2.setID('two');
       obj2.setPos(400,100);
-      obj2.setVel(0.2,0.0);
+      obj2.setVel(5,0.0);
       obj2.setColor('rgba(250, 50, 10, 0.5)');
       obj2.setMass(1);
       obj2.bind();
+      obj2.move(fps);
 
       let obj3 = this.get('obj3');
       obj3.setID('three');
       obj3.setPos(600,600);
-      obj3.setVel(0,-0.3);
+      obj3.setVel(0,-7);
       obj3.setColor('rgba(90, 255, 95, 0.5)');
       obj3.setMass(1);
       obj3.bind();
+      obj3.move(fps);
 
+
+    },
+    start() {
+      if (!this.get('objInit'))
+      {
+        this.send('reset');
+        addSquare(800,350,10,'darkblue','cm');
+        this.set('objInit',true);
+      }
+      this.set('isRunning',true);
       this.send('update');
     },
     stop(){
@@ -61,19 +64,22 @@ export default Controller.extend({
     },
     update(){
       let self = this;
+      let fps=this.get('framePerSec');
       later(this, function () {
+        //Update Display Info
         self.set('count', this.get('count') + 1);
-        self.set('sec',Math.floor(this.get('count')/this.get('loopPerSec')));
-        self.set('ms',this.get('count')%this.get('loopPerSec'));
+        self.set('sec',Math.floor(this.get('count')/fps));
+        self.set('ms',this.get('count')%fps);
 
-        //let position=$('#two').position();
-        //self.set('left',Math.round(position.left));
-        //self.set('top', Math.round(position.top));
+        //Simulate
+        self.get('obj1').move(fps);
+        self.get('obj2').move(fps);
+        self.get('obj3').move(fps);
+        self.get('obj1').draw();
+        self.get('obj2').draw();
+        self.get('obj3').draw();
 
-        self.get('obj1').move();
-        //self.get('obj2').move();
-        //self.get('obj3').move();
-
+        //Run the loop
         if (self.get('isRunning')){
           self.send('update');
         }
@@ -81,3 +87,13 @@ export default Controller.extend({
     }
   }
 });
+
+function addSquare(x,y,rad,color,id){
+  $('#graphContainer').append('<div' +
+    ' id="'+id+'" style="left: '+x+'px; top: '+y+'px;' +
+    ' width: '+rad+'px; height: '+rad+'px;' +
+    ' border-radius: '+'2'+'px; ' +
+    ' background-color: '+color+';' +
+    ' position: absolute;">' +
+    '</div>');
+}
--- app/obj.js
+++ app/obj.js
@@ -5,6 +5,8 @@ const Obj = EmberObject.extend({
   yPos: 0,
   xVel: 0,
   yVel: 0,
+  xForce: 0,
+  yForce: 0,
   color: "",
   mass: 1,
   radius:0,
@@ -35,7 +37,7 @@ const Obj = EmberObject.extend({
   bind(){
     if (!this.get('isBound')) {
       $('#graphContainer').append('<div' +
-        ' id="one" style="left: ' + this.get('xPos') + 'px; top: ' + this.get('yPos') + 'px;' +
+        ' id="'+this.get('id')+'" style="left: ' + this.get('xPos') + 'px; top: ' + this.get('yPos') + 'px;' +
         ' width: ' + this.get('radius') + 'px; height: ' + this.get('radius') + 'px;' +
         ' border-radius: ' + this.get('radius') + 'px; ' +
         ' background-color: ' + this.get('color') + ';' +
@@ -44,11 +46,21 @@ const Obj = EmberObject.extend({
       this.set('isBound',true);
     }
   },
-  move(){
+  draw(){
     let ID = '#' + this.get('id');
-    let position=$(ID).position();
-    $(ID).css('top', position.top+this.get('yVel'));
-    $(ID).css('left', position.left+this.get('xVel'));
+    $(ID).css('top', this.get('yPos'));
+    $(ID).css('left', this.get('xPos'));
+  },
+  move(fps){
+    this.set('xPos',this.get('xPos')+this.get('xVel')/fps);
+    this.set('yPos',this.get('yPos')+this.get('yVel')/fps);
+  },
+  resetForce(){
+    this.set('xForce',0);
+    this.set('yForce',0);
+  },
+  addForce(obj){
+
   }
 });
 
